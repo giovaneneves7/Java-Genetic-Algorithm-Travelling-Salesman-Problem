c583de957ab2a9143107dc5dca39cc0c02f9a4a0
giovaneneves7 <autornekoyasha@gmail.com>
2025-02-07 14:44:37 -0300

    first commit

---
+++ src/main/java/com/github/giovaneneves7/travellingsalesmanproblem/TravellingSalesmanProblem.java
@@ -0,0 +1,13 @@
+package com.github.giovaneneves7.travellingsalesmanproblem;
+
+/**
+ *
+ * @author Giovane Neves
+ */
+public class TravellingSalesmanProblem {
+
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/main/java/com/github/giovaneneves7/travellingsalesmanproblem/genes/TSPGene.java
@@ -0,0 +1,25 @@
+package com.github.giovaneneves7.travellingsalesmanproblem.genes;
+
+/**
+ * TSPGene's class
+ * @author Giovane Neves
+ */
+public class TSPGene {
+    
+    private final int x;
+    private final int y;
+
+    public TSPGene(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+    
+}
